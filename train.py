2e7980a9de4689c7ba10ae7e313c91595d898d26
swapna75 <konidenaswapna114@gmail.com>
2026-01-09 21:14:03 +0530

    Experiment 1: Model-LinearRegression, test_split-0.2, preprocessing-standard

--- train.py
+++ train.py
@@ -2,8 +2,8 @@ import pandas as pd
 import json
 import joblib
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error, r2_score
 
 data = pd.read_csv("dataset/winequality-red.csv", sep=';')
