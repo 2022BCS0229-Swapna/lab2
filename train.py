ed8c0cc3be675c4ef37104ceb15fb4cc85d137a6
swapna75 <konidenaswapna114@gmail.com>
2026-01-09 21:37:46 +0530

    Experiment 6: Model-RandomForest, feature-subset-selection

--- train.py
+++ train.py
@@ -2,37 +2,33 @@ import pandas as pd
 import json
 import joblib
 from sklearn.model_selection import train_test_split
-#from sklearn.linear_model import LinearRegression
-#from sklearn.linear_model import Ridge
-#from sklearn.linear_model import Lasso
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error, r2_score
 
 data = pd.read_csv("dataset/winequality-red.csv", sep=';')
 
-X = data.drop("quality", axis=1)
+selected_features = [
+    "alcohol",
+    "volatile acidity",
+    "sulphates",
+    "citric acid",
+    "density"
+]
+
+X = data[selected_features]
 y = data["quality"]
 
-scaler = StandardScaler()
-#X_scaled = scaler.fit_transform(X)
 X_scaled = X.values
-
 
 X_train, X_test, y_train, y_test = train_test_split(
     X_scaled, y, test_size=0.2, random_state=42
 )
 
-#model = LinearRegression()
-#model = Ridge(alpha=1.0)
-#model = Lasso(alpha=0.01)
-#model = RandomForestRegressor(n_estimators=100, random_state=42)
 model = RandomForestRegressor(
     n_estimators=200,
     max_depth=10,
     random_state=42
 )
-
 
 model.fit(X_train, y_train)
 
