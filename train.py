72cc486f9411746aa676dd617140923c37387700
swapna75 <konidenaswapna114@gmail.com>
2026-01-09 12:34:48 +0530

    Initial experiment: LinearRegression, StandardScaler

---
+++ train.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import json
+import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+
+data = pd.read_csv("dataset/winequality-red.csv", sep=';')
+
+X = data.drop("quality", axis=1)
+y = data["quality"]
+
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X_scaled, y, test_size=0.2, random_state=42
+)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+print(f"MSE: {mse}")
+print(f"R2: {r2}")
+
+joblib.dump(model, "output/model.pkl")
+
+results = {
+    "mse": mse,
+    "r2_score": r2
+}
+
+with open("output/results.json", "w") as f:
+    json.dump(results, f, indent=4)
